7727809f5a073f50bcf27a7f2303fc7619f5139f
camilapessina <capessinab@gmail.com>
2024-08-24 20:29:31 -0300

    pruebas

---
+++ js/login.js
@@ -0,0 +1,17 @@
+
+function login(event) {
+    event.preventDefault(); // Evita el comportamiento por defecto del formulario
+    
+    var username = document.getElementById('username').value; 
+    var password = document.getElementById('password').value; 
+
+    if (username && password) { 
+        // Guarda el estado de inicio de sesión en localStorage
+        localStorage.setItem('loggedIn', 'true'); 
+
+        // Redirige a index.html
+        window.location.href = "/index.html"; 
+    } else { 
+        alert('Por favor, complete ambos campos.'); 
+    } 
+}
---
+++ js/products.js
@@ -0,0 +1,38 @@
+const data = 'json/autos.json'; 
+
+fetch(data)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la red: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const contenedorJson = document.getElementById('contenedorJson');
+        contenedorJson.innerHTML = ''; 
+
+        data.products.forEach(product => {
+            // Crear un nuevo div para cada producto
+            const productoDiv = document.createElement('div');
+            productoDiv.className = 'producto';
+
+            // Agregar contenido al div
+            productoDiv.innerHTML = `
+            <div class="contenedor">    
+            <img class="auto" src="${product.image}" alt="${product.name}">
+                <div class="informacion">
+                    <p class="negrita">${product.name}</p>
+                    <p>${product.description}</p>
+                    <p>Vendidos: ${product.soldCount}</p>
+                </div>
+            </div>
+            <p class="precio">${product.currency} ${product.cost}</p>
+            `;
+
+            // Añadir el div al contenedor
+            contenedorJson.appendChild(productoDiv);
+        });
+    })
+    .catch(error => {
+        console.error('Hubo un problema con la solicitud:', error);
+    });
